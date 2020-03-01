78c2d16da193d4652f2abfc9ee76f955e120f235
Marton Papp <anteusz@freemail.hu>
2020-03-01 12:14:21 +0100

    changes for Windows... and to be able run in Visual Code

---
+++ build.bat
@@ -0,0 +1,8 @@
+
+
+
+
+cargo build --release
+@rem you need to be an administrator for the command to execute
+mklink siko.exe target\debug\siko.exe
+
---
+++ tests.bat
@@ -0,0 +1,9 @@
+
+
+call ./build
+
+mkdir -p comp
+mkdir -p rust_comp
+
+cd siko_tester
+cargo run -- D:\siko\siko.exe ../std ../comp ../rust_comp ../tests/success/ ../tests/fail/
